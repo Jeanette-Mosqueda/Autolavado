60c60fa2cb0a35c53c3acd48e9c2ac83f908f43d
Jeanette-Mosqueda <70932583+Jeanette-Mosqueda@users.noreply.github.com>
2022-05-13 08:03:35 -0700

    Add files via upload

---
+++ AutoLavado/AutoLavado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLavado
+//
+//  Created by user199389 on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
